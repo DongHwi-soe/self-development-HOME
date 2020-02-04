85546c13949cf1cc90b912f33c04e43dcb8a9f44
Dong Hui <dong>
2020-02-04 15:46:05 +0900

    변수

---
+++ JAVA_Programming/src/javaStudy/VariableExam.java
@@ -0,0 +1,23 @@
+package javaStudy;
+
+public class VariableExam {
+	
+	public static void main(String[] args) {
+		int count; // int : 타입(정수형), count : 이름
+		
+		count = 10;
+		
+		count = 20;
+		
+		System.out.println(count); // 마지막에 담긴 값만 남는다.
+		
+//		count = 11.1; int형에 double형을 담아서 컴파일에러 발생.
+		
+		double avg = 11.1;
+		
+		String name = "carami";
+		
+		int totalCount; // 두 개 이상의 단어가 만나면 두번째 이상부터 첫번째 단어 앞자리는 대문자로
+	}
+	
+}
