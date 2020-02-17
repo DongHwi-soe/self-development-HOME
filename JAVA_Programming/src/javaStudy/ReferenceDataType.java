a25fafff577cafb9266807f347696cb71feac46c
Dong Hui <dong>
2020-02-17 10:34:17 +0900

    참조타입

---
+++ JAVA_Programming/src/javaStudy/ReferenceDataType.java
@@ -0,0 +1,13 @@
+package javaStudy;
+
+public class ReferenceDataType {
+
+	public static void main(String[] args) {
+		int i = 4;
+		
+		String str = new String("Hello");
+		// str 변수는 String 인스턴스를 참조한다.
+
+	}
+
+}
