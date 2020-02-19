6dac0487b552fc6a3d9044dbbe39bb18af4afc08
Dong Hui <dong>
2020-02-19 10:07:47 +0900

    패키지

---
+++ JAVA_Programming/src/javaStudy/HelloExam.java
@@ -0,0 +1,12 @@
+package javaStudy;
+
+import com.eightcruz.javastudy.Hello;
+
+public class HelloExam {
+
+	public static void main(String[] args) {
+		Hello hello = new Hello(); // import 단축키 Ctrl + Shift + o
+//		com.eightcruz.javastudy.Hello hello = new com.eightcruz.javastudy.Hello(); // 불편하므로 이것은 안좋다.
+	}
+
+}
