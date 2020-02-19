74cf13b85dd6a8982174de9c6886e36a93fbff2c
Dong Hui <dong>
2020-02-19 15:19:41 +0900

    오버라이딩

--- JAVA_Programming/src/javaStudy/BusExam.java
+++ JAVA_Programming/src/javaStudy/BusExam.java
@@ -1,16 +1,9 @@
 package javaStudy;
 
 public class BusExam {
-
+	
 	public static void main(String[] args) {
 		Bus bus = new Bus();
 		bus.run();
-		bus.ppangppang();
-		
-		Car car = new Car();
-		car.run();
-		
-//		car.ppangppang();
 	}
-
 }
--- JAVA_Programming/src/javaStudy/Car.java
+++ JAVA_Programming/src/javaStudy/Car.java
@@ -1,7 +1,7 @@
 package javaStudy;
 
 public class Car {
-	public Car(String name) {
-		System.out.println("Car의 name 생성자입니다.");
+	public void run() {
+		System.out.println("Car의 run 메소드");
 	}
 }
