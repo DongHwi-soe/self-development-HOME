97ee28f62f962c384ca453c8ca68a828220ef18b
Dong Hui <dong>
2020-02-20 09:37:54 +0900

    클래스 형변환

--- JAVA_Programming/src/javaStudy/Bus.java
+++ JAVA_Programming/src/javaStudy/Bus.java
@@ -1,8 +1,7 @@
 package javaStudy;
 
 public class Bus extends Car { // public class 클래스명 extends 부모클래스명
-	public void run() {
-		super.run();
-		System.out.println("Bus의 run 메소드");
+	public void ppangppang() {
+		System.out.println("빵빵");
 	}
 }
--- JAVA_Programming/src/javaStudy/BusExam.java
+++ JAVA_Programming/src/javaStudy/BusExam.java
@@ -3,7 +3,13 @@ package javaStudy;
 public class BusExam {
 	
 	public static void main(String[] args) {
-		Bus bus = new Bus();
+		Car c = new Bus();
+		c.run();
+//		c.ppangppang(); // 부모 타입으로 자식을 가리킬 수 있다 이 경우 부모가 가지고 있는 내용만 사용 가능
+		
+		Bus bus = (Bus)c;
 		bus.run();
+		bus.ppangppang();
 	}
+	
 }
