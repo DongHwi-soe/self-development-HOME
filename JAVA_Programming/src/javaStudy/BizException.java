6c188f4213203f996ad74a9a5696e7ae8462072c
Dong Hui <dong>
2020-02-24 10:56:09 +0900

    사용자 정의 Exception

---
+++ JAVA_Programming/src/javaStudy/BizException.java
@@ -0,0 +1,11 @@
+package javaStudy;
+
+public class BizException extends RuntimeException {
+
+	public BizException(String msg) {
+		super(msg);
+	}
+	public BizException(Exception ex) { 
+		super(ex);
+	}
+}
